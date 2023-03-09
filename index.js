07d35c912bcee9791b4073da1434aed42576d2b6
l1zondo <lizondofrancoezequiel@gmail.com>
2023-03-09 19:54:26 -0300

    problema solucionado proyecto terminado

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const degree = 6;
 const hr = document.querySelector('#hr');
 const min = document.querySelector('#min');
-const sec = document.querySelector('sec');
+const sec = document.querySelector('#sec');
 
 setInterval(() => {
     
